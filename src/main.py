c85548dd922a1cfe60d741684cdaae4670ee2ea6
Salman Shaikh <tony903212@gmail.com>
2021-11-08 15:52:21 +0530

    Added docstrings and minor fixes

--- src/main.py
+++ src/main.py
@@ -1,12 +1,4 @@
-from fastapi.middleware.cors import CORSMiddleware
-from fastapi import FastAPI
-from typing import Union
-from enum import Enum
-import requests
-import json
-import re
-
-description = """
+"""
 Unofficial Fiverr API helps you to get:
 
 * Seller details
@@ -18,6 +10,16 @@ Unofficial Fiverr API helps you to get:
     * Limit no. of reviews
 """
 
+from typing import Union
+from enum import Enum
+import json
+import re
+
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI
+import requests
+
+DESCRIPTION = __doc__ or ""
 tags_metadata = [
     {
         "name": "Home"
@@ -30,7 +32,7 @@ tags_metadata = [
 
 app = FastAPI(
     title="Unofficial Fiverr API",
-    description=description,
+    description=DESCRIPTION,
     version="1.2",
     contact={
         "name": "Salman Shaikh",
@@ -50,22 +52,24 @@ app.add_middleware(
 
 
 class FilterBy(str, Enum):
-    positive = "positive"
-    negative = "negative"
+    """
+    Filter by Enum Class
+    """
+    POSITIVE = "positive"
+    NEGATIVE = "negative"
 
 
 class SortBy(str, Enum):
-    recent = "recent"
-    relevant = "relevant"
+    """
+    Sort by Enum Class
+    """
+    RECENT = "recent"
+    RELEVANT = "relevant"
 
 
 headers = {
     "User-Agent":
     "Mozilla/5.0 (X11; Linux x86_64; rv:90.0) Gecko/20100101 Firefox/90.0",
-    "Sec-Fetch-Dest": "document",
-    "Sec-Fetch-Mode": "navigate",
-    "Sec-Fetch-Site": "none",
-    "Sec-Fetch-User": "?1",
     "Upgrade-Insecure-Requests": "1",
     "TE": "trailers"
 }
@@ -81,18 +85,24 @@ reviews_headers = {
     "TE": "trailers"
 }
 
-
 def get_user_data(username: str):
+    """
+    Get basic seller details and CSRF Token
+    """
+    session = requests.Session()
     data_regex = r'(?<=perseusApp.initialData = ).+(?=;)'
     seller_url = f"https://www.fiverr.com/{username}"
-    data = requests.get(seller_url, headers=headers)
+    data = session.get(seller_url, headers=headers)
     react_data = re.findall(data_regex, data.text)[0]
     seller_data = json.loads(react_data)
-    return seller_data
+    return session, seller_data
 
 
 @app.get("/", tags=["Home"])
 async def index():
+    """
+    Home Path for API
+    """
     return {
         "Welcome to": "Unofficial Fiverr API",
         "For docs": "Visit /docs",
@@ -102,7 +112,10 @@ async def index():
 
 @app.get("/{username}", tags=["Seller Details"])
 async def get_seller_details(username: str):
-    user_data = get_user_data(username)
+    """
+    Remove unnecessary details from seller card and returns it
+    """
+    session, user_data = get_user_data(username)
     seller_card = user_data["userData"]["seller_card"]
     seller_profile = user_data["userData"]["seller_profile"]
     seller_card.update(seller_profile)
@@ -111,21 +124,30 @@ async def get_seller_details(username: str):
     seller_card.pop("identities")
     seller_card.pop("currency")
     seller_card.pop("abTests")
+    session.close()
     return seller_card
 
 
 @app.get("/{username}/gigs", tags=["Seller Details"])
 async def get_gigs(username: str):
-    user_data = get_user_data(username)
+    """
+    Get seller gigs
+    """
+    session, user_data = get_user_data(username)
+    session.close()
     return user_data["gigs"]["gigs"]
 
 
 @app.get("/{username}/reviews", tags=["Seller Details"])
-async def get_reviews(username: str, filter_by: Union[FilterBy, None]  = None ,
+async def get_reviews(username: str, filter_by: Union[FilterBy, None] = None,
                       sort_by: Union[SortBy, None] = None, group_by_buyer: bool = False,
                       limit: int = 9999):
+    """
+    Get seller reviews
+    """
     url = "https://www.fiverr.com/ratings/index"
-    user_data = get_user_data(username)
+    session, user_data = get_user_data(username)
+
     # Adding CSRF Token
     reviews_headers["X-CSRF-Token"] = user_data["requestContext"]["csrf_token"]
     # Setting up payload
@@ -136,8 +158,9 @@ async def get_reviews(username: str, filter_by: Union[FilterBy, None]  = None ,
     if sort_by:
         payload["sort_by"] = sort_by.value
     reviews: list[dict[str, str]] = []
+
     while True:
-        data = requests.get(url, headers=headers, data=payload)
+        data = session.get(url, headers=headers, data=payload)
         data = data.json()
         reviews.extend(data["reviews"])
         if not data["has_next"] or len(reviews) >= limit:
@@ -147,9 +170,10 @@ async def get_reviews(username: str, filter_by: Union[FilterBy, None]  = None ,
     if group_by_buyer:
         merged_reviews: dict[str, list[dict[str, str]]] = {}
         for review in reviews:
-            if review["username"] in merged_reviews.keys():
+            if review["username"] in merged_reviews:
                 merged_reviews[review["username"]].append(review)
             else:
                 merged_reviews[review["username"]] = [review]
         return merged_reviews
+    session.close()
     return reviews
