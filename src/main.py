2bde34c572511c044395be34fe5d203ce1c660ba
Salman Shaikh <tony903212@gmail.com>
2022-06-16 17:50:47 +0530

    Fixed Server Error 500
    Fiverr recently updated their website so it broke the API this commit fixes that
    solves #1

--- src/main.py
+++ src/main.py
@@ -17,6 +17,7 @@ import re
 
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
+from bs4 import BeautifulSoup
 import requests
 
 DESCRIPTION = __doc__ or ""
@@ -69,15 +70,16 @@ class SortBy(str, Enum):
 
 headers = {
     "User-Agent":
-    "Mozilla/5.0 (X11; Linux x86_64; rv:90.0) Gecko/20100101 Firefox/90.0",
-    "Upgrade-Insecure-Requests": "1",
-    "TE": "trailers"
+    "Mozilla/5.0 (X11; Linux x86_64; rv:90.0) Gecko/20100101 Firefox/101.0",
+    "Sec-Fetch-Dest": "document",
+    "Sec-Fetch-Mode": "navigate",
+    "Sec-Fetch-Site": "none",
 }
 reviews_headers = {
     "User-Agent":
     "Mozilla/5.0 (X11; Linux x86_64; rv:90.0) Gecko/20100101 Firefox/90.0",
     "Sec-Fetch-Dest": "empty",
-    "Sec-Fetch-Mode": "no-cors",
+    "Sec-Fetch-Mode": "cors",
     "Sec-Fetch-Site": "same-origin",
     "X-Requested-With": "XMLHttpRequest",
     "Accept": "application/json",
@@ -85,16 +87,19 @@ reviews_headers = {
     "TE": "trailers"
 }
 
+
 def get_user_data(username: str):
     """
     Get basic seller details and CSRF Token
     """
     session = requests.Session()
-    data_regex = r'(?<=perseusApp.initialData = ).+(?=;)'
     seller_url = f"https://www.fiverr.com/{username}"
     data = session.get(seller_url, headers=headers)
-    react_data = re.findall(data_regex, data.text)[0]
-    seller_data = json.loads(react_data)
+    soup = BeautifulSoup(data.text, "lxml")
+    # TODO: Take a look at Sec-Fetch-Dest Sec-Fetch-Mode Sec-Fetch-Site
+    # print(soup.find("script", id="perseus-initial-props").get_text())
+    seller_data = json.loads(soup.find("script", id="perseus-initial-props").string or "null")
+    seller_data["csrfToken"] = soup.find("meta", {"property": "csrfToken"}).get("content")
     return session, seller_data
 
 
@@ -119,11 +124,6 @@ async def get_seller_details(username: str):
     seller_card = user_data["userData"]["seller_card"]
     seller_profile = user_data["userData"]["seller_profile"]
     seller_card.update(seller_profile)
-    seller_card.pop("biEventData")
-    seller_card.pop("collectProps")
-    seller_card.pop("identities")
-    seller_card.pop("currency")
-    seller_card.pop("abTests")
     session.close()
     return seller_card
 
@@ -145,11 +145,12 @@ async def get_reviews(username: str, filter_by: Union[FilterBy, None] = None,
     """
     Get seller reviews
     """
-    url = "https://www.fiverr.com/ratings/index"
     session, user_data = get_user_data(username)
+    url = f"https://www.fiverr.com/reviews/user_page/fetch_user_reviews/{user_data['userData']['user']['id']}"
 
     # Adding CSRF Token
-    reviews_headers["X-CSRF-Token"] = user_data["requestContext"]["csrf_token"]
+    reviews_headers["X-CSRF-Token"] = user_data["csrfToken"]
+    reviews_headers["Referer"] = f"https://www.fiverr.com/{username}"
     # Setting up payload
     payload: dict[str, str] = {}
     payload["user_id"] = user_data["userData"]["user"]["id"]
@@ -160,12 +161,13 @@ async def get_reviews(username: str, filter_by: Union[FilterBy, None] = None,
     reviews: list[dict[str, str]] = []
 
     while True:
-        data = session.get(url, headers=headers, data=payload)
+        data = session.get(url, headers=headers, params=payload)
         data = data.json()
         reviews.extend(data["reviews"])
         if not data["has_next"] or len(reviews) >= limit:
             break
         payload["last_star_rating_id"] = reviews[-1]["id"]
+        payload["last_review_id"] = reviews[-1]["id"]
     reviews = reviews[:limit]
     if group_by_buyer:
         merged_reviews: dict[str, list[dict[str, str]]] = {}
