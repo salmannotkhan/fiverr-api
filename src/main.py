aeadd94817ef4817e8a186b7e14e386549c2ff94
Salman Shaikh <tony903212@gmail.com>
2022-10-08 03:20:56 +0530

    fix: bypassing cloudflare bot protection

--- src/main.py
+++ src/main.py
@@ -18,6 +18,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 from bs4 import BeautifulSoup
 import requests
+import cloudscraper
 
 DESCRIPTION = __doc__ or ""
 tags_metadata = [
@@ -67,13 +68,6 @@ class SortBy(str, Enum):
     RELEVANT = "relevant"
 
 
-headers = {
-    "User-Agent":
-    "Mozilla/5.0 (X11; Linux x86_64; rv:90.0) Gecko/20100101 Firefox/101.0",
-    "Sec-Fetch-Dest": "document",
-    "Sec-Fetch-Mode": "navigate",
-    "Sec-Fetch-Site": "none",
-}
 reviews_headers = {
     "User-Agent":
     "Mozilla/5.0 (X11; Linux x86_64; rv:90.0) Gecko/20100101 Firefox/90.0",
@@ -81,9 +75,6 @@ reviews_headers = {
     "Sec-Fetch-Mode": "cors",
     "Sec-Fetch-Site": "same-origin",
     "X-Requested-With": "XMLHttpRequest",
-    "Accept": "application/json",
-    "Upgrade-Insecure-Requests": "1",
-    "TE": "trailers"
 }
 
 
@@ -91,19 +82,13 @@ def get_user_data(username: str):
     """
     Get basic seller details and CSRF Token
     """
-    try: 
-        session = requests.Session()
-        seller_url = f"https://www.fiverr.com/{username}"
-        data = session.get(seller_url, headers=headers)
-        soup = BeautifulSoup(data.text, "lxml")
-        # print(soup.find("script", id="perseus-initial-props").get_text())
-        seller_data = json.loads(soup.find("script", id="perseus-initial-props").string or "null")
-        seller_data["csrfToken"] = soup.find("meta", {"property": "csrfToken"}).get("content")
-        return session, seller_data
-    except Exception:
-        print(data)
-        print(data.text)
-        print(soup.find("script", id="perseus-initial-props"))
+    scraper = cloudscraper.create_scraper()
+    seller_url = f"https://www.fiverr.com/{username}"
+    data = scraper.get(seller_url)
+    soup = BeautifulSoup(data.text, "lxml")
+    seller_data = json.loads(soup.find("script", id="perseus-initial-props").string or "null")
+    seller_data["csrfToken"] = soup.find("meta", {"property": "csrfToken"}).get("content")
+    return scraper.cookies, seller_data
 
 
 @app.get("/", tags=["Home"])
@@ -123,11 +108,10 @@ async def get_seller_details(username: str):
     """
     Remove unnecessary details from seller card and returns it
     """
-    session, user_data = get_user_data(username)
+    _, user_data = get_user_data(username)
     seller_card = user_data["userData"]["seller_card"]
     seller_profile = user_data["userData"]["seller_profile"]
     seller_card.update(seller_profile)
-    session.close()
     return seller_card
 
 
@@ -136,8 +120,7 @@ async def get_gigs(username: str):
     """
     Get seller gigs
     """
-    session, user_data = get_user_data(username)
-    session.close()
+    _, user_data = get_user_data(username)
     return user_data["gigs"]["gigs"]
 
 
@@ -148,7 +131,9 @@ async def get_reviews(username: str, filter_by: Union[FilterBy, None] = None,
     """
     Get seller reviews
     """
-    session, user_data = get_user_data(username)
+    session = requests.session()
+    cookies, user_data = get_user_data(username)
+    session.cookies = cookies
     url = f"https://www.fiverr.com/reviews/user_page/fetch_user_reviews/{user_data['userData']['user']['id']}"
 
     # Adding CSRF Token
@@ -163,14 +148,16 @@ async def get_reviews(username: str, filter_by: Union[FilterBy, None] = None,
         payload["sort_by"] = sort_by.value
     reviews: list[dict[str, str]] = []
 
+    scraper = cloudscraper.create_scraper(sess=session, browser="chrome")
     while True:
-        data = session.get(url, headers=headers, params=payload)
+        data = scraper.get(url, headers=reviews_headers, params=payload)
         data = data.json()
         reviews.extend(data["reviews"])
         if not data["has_next"] or len(reviews) >= limit:
             break
         payload["last_star_rating_id"] = reviews[-1]["id"]
         payload["last_review_id"] = reviews[-1]["id"]
+        payload["last_score"] = "0"
     reviews = reviews[:limit]
     if group_by_buyer:
         merged_reviews: dict[str, list[dict[str, str]]] = {}
